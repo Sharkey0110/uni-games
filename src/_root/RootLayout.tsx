b10bfdace238809433854b1539488a7839228a70
Jack Sharkey <113288400+Sharkey0110@users.noreply.github.com>
2026-03-22 19:55:17 +0000

    initial setup

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
     <div>
       <p>Root layout</p>
+      <Outlet />
     </div>
   )
 }
